1373422283a4207dcd18584463c9c6e4b424bfdd
José Divino Jr. <josedivinojr19@gmail.com>
2025-08-23 13:54:28 -0300

    feat(entity): adds client entity and tests

---
+++ wallet/entity/client_test.go
@@ -0,0 +1,46 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateNewClient(t *testing.T) {
+	client, err := NewClient("John", "john@john.com")
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.Equal(t, "John", client.Name)
+	assert.Equal(t, "john@john.com", client.Email)
+}
+
+func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
+	client, err := NewClient("", "")
+
+	assert.NotNil(t, err)
+	assert.Nil(t, client)
+}
+
+func TestUpdateClient(t *testing.T) {
+
+	client, _ := NewClient("John", "john@john.com")
+
+	err := client.Update("Updated John", "updated@john.com")
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, "Updated John", client.Name)
+
+	assert.Equal(t, "updated@john.com", client.Email)
+
+}
+
+func TestUpdateClientWithInvalidArgs(t *testing.T) {
+	client, _ := NewClient("John", "john@john.com")
+
+	err := client.Update("", "updated@john.com")
+	assert.Error(t, err, "name is required")
+
+	err = client.Update("Updated John", "")
+	assert.Error(t, err, "email is required")
+}
