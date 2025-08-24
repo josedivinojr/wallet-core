b93dd2c449a734ceffd8d7db3da8cd457c0d47e7
José Divino Jr. <josedivinojr19@gmail.com>
2025-08-23 23:29:17 -0300

    feat(database): updates account db tests

--- wallet/internal/database/account_db_test.go
+++ wallet/internal/database/account_db_test.go
@@ -6,33 +6,59 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type AccountDBTestSuite struct {
 	suite.Suite
 	db        *sql.DB
 	accountDB *AccountDB
-	client    *entity.Client
+	clientDB  *ClientDB
 }
 
 func (s *AccountDBTestSuite) SetupSuite() {
 	db, err := sql.Open("sqlite3", ":memory:")
 	s.Nil(err)
 	s.db = db
-
-	db.Exec("Create table clients (id VARCHAR(255), name VARCHAR(255), email VARCHAR(255), created_at DATE)")
-	db.Exec("Create table accounts (id VARCHAR(255), client_id VARCHAR(255), balance int, created_at DATE)")
-
+	db.Exec("CREATE TABLE clients (id VARCHAR(255), name VARCHAR(255), email VARCHAR(255), created_at DATE)")
+	db.Exec("CREATE TABLE accounts (id VARCHAR(255), client_id VARCHAR(255), balance INTEGER, created_at DATE)")
 	s.accountDB = NewAccountDB(db)
-	s.client, _ = entity.NewClient("John", "j@j.com")
+	s.clientDB = NewClientDb(db)
 }
 
 func (s *AccountDBTestSuite) TearDownSuite() {
 	defer s.db.Close()
-	s.db.Exec("DROP TABLE clients")
 	s.db.Exec("DROP TABLE accounts")
+	s.db.Exec("DROP TABLE clients")
 }
 
 func TestAccountDBTestSuite(t *testing.T) {
 	suite.Run(t, new(AccountDBTestSuite))
+}
+
+func (s *AccountDBTestSuite) TestSave() {
+	client, _ := entity.NewClient("John", "j@j.com")
+	s.clientDB.Save(client)
+
+	account := entity.NewAccount(client)
+	err := s.accountDB.Save(account)
+	s.Nil(err)
+}
+
+func (s *AccountDBTestSuite) TestGetById() {
+	client, _ := entity.NewClient("John", "j@j.com")
+	s.clientDB.Save(client)
+
+	account := entity.NewAccount(client)
+	account.Credit(100)
+	s.accountDB.Save(account)
+
+	accountDB, err := s.accountDB.GetById(account.ID)
+	s.Nil(err)
+	s.Equal(account.ID, accountDB.ID)
+	s.Equal(account.Balance, accountDB.Balance)
+	s.Equal(account.Client.ID, accountDB.Client.ID)
+	s.Equal(account.Client.Name, accountDB.Client.Name)
+	s.Equal(account.Client.Email, accountDB.Client.Email)
 }
